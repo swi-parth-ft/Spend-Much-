b600200ab60b2f33cd153d3352a492a763c370f5
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-08 19:15:06 -0400

    Initial Commit

---
+++ Spend Much?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spend Much?
+//
+//  Created by Parth Antala on 2024-07-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spend Much?/Spend_Much_App.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_Much_App.swift
+//  Spend Much?
+//
+//  Created by Parth Antala on 2024-07-08.
+//
+
+import SwiftUI
+
+@main
+struct Spend_Much_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
