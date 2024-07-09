870dd27f840b99ee5eada408fc14b21a860c8e77
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-08 20:25:20 -0400

    Try Codable

--- Spend Much?/ContentView.swift
+++ Spend Much?/ContentView.swift
@@ -13,6 +13,11 @@ class User {
     var firstName = "Parth"
     var lastName = "Antala"
 }
+
+struct Book: Codable {
+    var name: String
+    var isnb: Double
+}
 struct ContentView: View {
     
     @State private var user = User()
@@ -20,6 +25,10 @@ struct ContentView: View {
     @State private var numbers: [Int] = []
     @State private var nextNumber: Int = 1
     @AppStorage("tapCount") private var tapCount = 0
+    
+    
+    @State private var GOT: Book = Book(name: "GameofThrons", isnb: 82746724574)
+    @State private var got: Book?
     
     var body: some View {
         NavigationStack {
@@ -33,8 +42,15 @@ struct ContentView: View {
                 
                 Button("Present sheet") {
                     isPresented = true
+                    let decoder = JSONDecoder()
+                    
+                    if let data = UserDefaults.standard.data(forKey: "Books") {
+                        if let decodedBook = try? decoder.decode(Book.self, from: data) {
+                            got = decodedBook
+                        }
+                    }
                 }.sheet(isPresented: $isPresented) {
-                    secondView()
+                    secondView(book: got ?? Book(name: "non", isnb: 82746724574))
                 }
                 
                 List {
@@ -44,7 +60,14 @@ struct ContentView: View {
                     }
                     .onDelete(perform: removeRow)
                 }
-                
+                Button("Add Book") {
+                    let encoder = JSONEncoder()
+                    
+                    if let data = try? encoder.encode(GOT) {
+                        UserDefaults.standard.set(data, forKey: "Books")
+                    }
+                }
+
                 Button("Add number") {
                     numbers.append(tapCount)
                     tapCount += 1
@@ -68,10 +91,10 @@ struct ContentView: View {
 
 struct secondView: View {
     @Environment(\.dismiss) var dismiss
-    
+    var book: Book
     var body: some View {
         VStack {
-            Text("Second View")
+            Text(book.name)
             Button("dismiss") {
                 dismiss()
             }
